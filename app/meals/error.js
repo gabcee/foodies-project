190d6b702119fad2818b84192038b57cd598e784
gabcaraos <gmcaraos@stratodigital.io>
2025-01-20 09:55:56 +0800

    added a db to store new meals

--- app/meals/error.js
+++ app/meals/error.js
@@ -3,6 +3,6 @@
 export default function Error() {
     return <main className="error">
         <h1>An error occured!</h1>
-        <p>Failed to fetch meal data. Try to fetch again later.</p>
+        <p>Failed to create meal.</p>
     </main>
 }
